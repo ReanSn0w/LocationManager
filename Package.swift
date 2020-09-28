7406cc16734369b00138cff2d3799a80348e5ae7
Дмитрий Папков <re@nsnow.xyz>
2020-09-28 15:29:27 +0300

    Update Package.swift
    Add SupportedPlatform

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "LocationManager",
+	platforms: [.iOS(.v13)],
     products: [
         .library(name: "LocationManager",targets: ["LocationManager"]),
     ],
